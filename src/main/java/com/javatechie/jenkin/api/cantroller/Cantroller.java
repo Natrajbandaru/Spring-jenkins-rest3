4b74719df04f813212f8fdddee18da86c570f510
Natrajbandaru <natrajbandaru99@gmail.com>
2021-12-13 16:56:25 +0530

    initalcommit

---
+++ src/main/java/com/javatechie/jenkin/api/cantroller/Cantroller.java
@@ -0,0 +1,24 @@
+package com.javatechie.jenkin.api.cantroller;
+
+import javax.annotation.PostConstruct;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Cantroller {
+	
+	@PostMapping(value="hello",produces = "application/json")
+	public String testing() {
+		
+		JSONObject jsonobject=new JSONObject();
+		
+		jsonobject.put("status", "Ok");
+		jsonobject.put("message", "printed");
+		
+		return jsonobject.toString() ;
+	}
+	
+}
